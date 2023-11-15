3205d9ae2b83b87b5123937e2a2eb44fe5ade561
ИСП-31 <-31@LAB17-04>
2023-11-15 10:00:25 +0300

    Добавлены комментарии к методам класса

--- Lib_4/WorkerFather.cs
+++ Lib_4/WorkerFather.cs
@@ -17,17 +17,31 @@ namespace Lib_4
             Children = children;
         }
 
+        /// <summary>
+        /// Сравнение фамилий двух работников
+        /// </summary>
+        /// <param name="obj">Фамилия одного из работников</param>
+        /// <returns>0, если фамилии одинаковы; 1 или -1, если фамилии различаются</returns>
         public int CompareTo(object obj)
         {
             WorkerFather temp = (WorkerFather)obj;
             return String.Compare(this.LastName, temp.LastName);
         }
         
+        /// <summary>
+        /// Клонирование работника
+        /// </summary>
+        /// <returns>Работник с теми же фамилией, именем, профессией и числом детей в семье</returns>
         public object Clone()
         {
             return new WorkerFather(this.FirstName, this.LastName, this.Job, this.Children);
         }
         
+        /// <summary>
+        /// Вывод информации о работнике
+        /// </summary>
+        /// <param name="n">Работник</param>
+        /// <returns>Информация о работнике</returns>
         public static string GetInfo(WorkerFather n)
         {
             return $"{n.FirstName} {n.LastName}, профессия - {n.Job}, детей в семье - {n.Children}";
